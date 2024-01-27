420140948de40895bdd6ebf29a2d899a8e32aed3
SRIDHAR S <149185042+sridharkpr45@users.noreply.github.com>
2024-01-27 21:46:50 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,41 @@
+let displayValue = '0';
+
+function updateDisplay() {
+    document.getElementById('display').innerText = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '0';
+    updateDisplay();
+}
+
+function appendNumber(number) {
+    if (displayValue === '0') {
+        displayValue = number;
+    } else {
+        displayValue += number;
+    }
+    updateDisplay();
+}
+
+function appendOperator(operator) {
+    displayValue += operator;
+    updateDisplay();
+}
+
+function appendDecimalPoint() {
+    if (!displayValue.includes('.')) {
+        displayValue += '.';
+        updateDisplay();
+    }
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue).toString();
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        updateDisplay();
+    }
+}
